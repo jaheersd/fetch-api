1e32484e40f346bb2b71fdd747a84d42baa829c9
jaheersd <jaheer5667@gmail.com>
2024-06-06 18:59:00 +0530

    added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { useEffect } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const FetchApi = () => {
+  const API_ENDPOINT = "https://jsonplaceholder.typicode.com/users";
+  const fetchUsers = async (uri) => {
+    try {
+      const response = await fetch(uri);
+      const data = await response.json();
+      console.log(data);
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  useEffect(() => {
+    fetchUsers(API_ENDPOINT);
+  }, []);
+  return <div>fetch api practice </div>;
+};
 
-export default App;
+export default FetchApi;
